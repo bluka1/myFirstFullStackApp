f31a93088ff11d88466b1701d3660ccbc28706d2
bluka1 <luka.batarelo@gmail.com>
2023-06-29 23:24:26 +0200

    master - added spring security

---
+++ src/main/java/com/bluka1/springboot/myfirstfullstackapp/login/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.bluka1.springboot.myfirstfullstackapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String goToWelcomePage(ModelMap model) {
+		model.put("name", "bluka1");
+		return "welcome";
+	}
+}
